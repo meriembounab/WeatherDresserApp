436b630cbf42539df7c4bfd92acc895a5c831ede
Meriem <mbounab@purdue.edu>
2018-12-04 15:22:40 -0500

    set up basics of app

---
+++ local-production.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: {
+      uri: 'mongodb://<cosmosdb_name>:xAceMy060oVfHcdf4040QGHk1YfJuwxLzrpvN1SYA0miFZLbhLiM05U91oLFPfjdhmDQm8vnbIRa9wjf0xxwNA==@weatherdresser.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false'
+    }
+  };
