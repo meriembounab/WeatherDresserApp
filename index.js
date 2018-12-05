975b203ca4f16af12476d5717452ff62d91acbb1
Meriem <mbounab@purdue.edu>
2018-12-04 20:54:28 -0500

    added files for additional pages

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ var server = http.createServer(function(request, response) {
 
 });
 
+app.use("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+   });
+
+
 var port = process.env.PORT || 1337;
+
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
