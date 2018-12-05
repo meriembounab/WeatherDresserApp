73faa74278de54c500178b2bfb724353a4428071
Meriem <mbounab@purdue.edu>
2018-12-05 10:50:12 -0500

    added sign in sign up inputs

--- js/index.js
+++ js/index.js
@@ -14,7 +14,16 @@ app.use("/", (req, res) => {
     res.sendFile("/index.html");
    });
 
-   
+   app.post("/getDressed.html", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+    .then(item => {
+    res.send("/getDressed.html");
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+    });
+   });
 var port = process.env.PORT || 1337;
 
 server.listen(port);
